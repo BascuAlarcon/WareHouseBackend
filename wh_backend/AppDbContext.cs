e7d70f376a856b1a4395a0e779d711563b6917b6
BascuAlarcon <bascualarconalexander@gmail.com>
2021-09-24 15:19:39 -0300

    Add project files.

---
+++ wh_backend/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wh_backend.Models;
+
+namespace wh_backend
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Item> Item { get; set; }
+    }
+}
---
+++ wh_backend/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wh_backend.Models
+{
+    public class Item
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string name { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string description { get; set; }
+        [Required]
+        [Column(TypeName = "int")]
+        public int category { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public string section { get; set; }
+    }
+}
